9c14c591d24aa6cdaf255384f1c56dc77b243180
Manuel Morales <manolin@mit.edu>
2020-01-27 12:49:45 -0600

    Update alfiles_14.js

---
+++ js/alfiles_14.js
@@ -0,0 +1,14 @@
+var Chess = require('./chess').Chess;
+
+rnd_start = rnd_fen(14 alfiles) // waiting for rnd_fen method that can pass in 14 alfiles
+var chess = new Chess(rnd_start);
+chess.ascii(); // show board
+
+solns = ['BBBBBBB1/8/8/8/8/8/8/BBBBBBB1', '1BBBBBBB/8/8/8/8/8/8/1BBBBBBB']
+while (!solns.includes(chess.fen().slice(0,29))) { // runs until current fen string is a soln
+  var moves = chess.moves();
+  var move = moves[Math.floor(Math.random() * moves.length)]; // need to change from random to user input
+  chess.move(move);
+}
+console.log(chess.pgn());
+
