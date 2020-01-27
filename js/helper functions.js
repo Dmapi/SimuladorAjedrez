f8a3e4711536e11cceadf630b37d9ea4f02a66a3
Manuel Morales <manolin@mit.edu>
2020-01-27 17:15:41 -0600

    consolidated posiciones aleatorias with helper functions

--- js/helper functions.js
+++ js/helper functions.js
@@ -1,7 +1,38 @@
+//define pieces as objects
+var wR = {type: 'r', color:'w'}
+var wB = {type:'b', color:'w'}
+var wK = {type:'k', color:'w'}
+var wN = {type:'n', color:'w'}
+var wQ = {type:'q', color:'w'}
+var wP = {type:'p', color:'w'}
+var bR = {type:'r', color:'b'}
+var bB = {type:'b',color:'b'}
+var bK = {type:'k', color:'b'}
+var bN = {type:'n', color:'b'}
+var bQ = {type:'q', color:'b'}
+var bP = {type:'p', color:'b'}
 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f','g'];
 var numbers = ['1','2','3','4','5', '6', '7', '8'];
 
+function allPieces() { //function to generate array of all pieces
+    pieces = [wK, wQ, bK, bQ]
+
+    for (i = 0; i < 2; i++) {
+        allPieces.push(wR)
+        allPieces.push(wB)
+        allPieces.push(wN)
+        allPieces.push(bR)
+        allPieces.push(bB)
+        allPieces.push(bN)
+    }
+    
+    for (i = 0; i < 8; i++) {
+        allPieces.push(wP)
+        allPieces.push(bP)
+    }
+    return pieces    
+}
 function create_squares(){ // creates square objects with color
     var squares = []; 
 
@@ -30,7 +61,25 @@ function full_squares(){ // returns squares with pieces on them
     }
     return full_squares
 }
+function randomPositions(pieces, squares) { //place pieces with random squares and pieces
+    positionsArray = []
+    for (i = 0; i < pieces.length; i++) {
+        // find random piece and square by index
+        var pieceIndex = math.floor(math.random() * pieces.length)
+        var squareIndex = math.foor(math.random() * squares.length)
+    
+        chess.put(pieces[pieceIndex], squares[squareIndex]) //places piece in random position
+    
+        //add square and piece pair to positionsArray
+        newPosition = [pieces[pieceIndex], squares[squareIndex]]
+        positionsArray.push(newPosition)
 
+        squares.splice(squareIndex, 1) //removes selected square
+        pieces.splice(pieceIndex, 1) //removes selected piece
+    
+    return positionsArray
+    }
+}
 var full_squares = full_squares()
 var num_squares = full_squares.length
 
