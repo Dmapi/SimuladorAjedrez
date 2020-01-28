0d57e1ddc4dd79fef7dadb8d5bbf89dedc5d24b7
Manuel Morales <manolin@mit.edu>
2020-01-28 14:10:51 -0600

    Update damas_de_gauss.js

--- js/damas_de_gauss.js
+++ js/damas_de_gauss.js
@@ -1,14 +1,12 @@
-var Chess = require('./chess').Chess;
-
 pieces = [wQ, wQ, wQ, wQ, wQ, wQ, wQ, wQ];
 squares = create_squares();
 rnd_start = randomPositions(pieces, squares);
-var chess = new Chess(rnd_start);
-chess.ascii(); // show board
 
-while (!fila_y_columna() & !diagonal()) { // runs until all queens on different row, column, and diagonal
-  var moves = chess.moves();
-  var move = moves[Math.floor(Math.random() * moves.length)]; // need to change from random to user input
-  chess.move(move);
+var Chess = require('./chess').Chess;
+var chess = new Chess(rnd_start);
+
+while (!fila_y_columna() ||  !diagonal()) { // runs until all queens on different row, column, and diagonal
+  // play versus person or computer
 }
+
 console.log(chess.pgn());
