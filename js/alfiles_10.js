9b094f62445074714afb589cd93648b8141f7426
Manuel Morales <manolin@mit.edu>
2020-01-28 13:29:49 -0600

    added more solns to alfiles 10

--- js/alfiles_10.js
+++ js/alfiles_10.js
@@ -6,7 +6,7 @@ var Chess = require('./chess').Chess;
 var chess = new Chess(rnd_start);
 
 solns = ['8/3BB3/8/2B2B2/2B2B2/2B2B2/3BB3/8', '8/3BB3/2B2B2/2B2B2/2B2B2/8/3BB3/8',
-          '', '']; // 4 solns
+          '8/8/3BBB2/1B4B1/1B4B1/3BBB2/8/8', '8/8/2BBB3/1B4B1/1B4B1/2BBB3/8/8']; // 4 solns
 while (!solns.includes(chess.fen().slice(0,33))) { // runs until current fen string is a soln
   // play versus person or computer
 }
