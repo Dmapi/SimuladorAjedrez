93f437812bf6e79ef9261b867d1410a98b04fc89
Manuel Morales <manolin@mit.edu>
2020-01-28 14:06:32 -0600

    Update torres_8.js

--- js/torres_8.js
+++ js/torres_8.js
@@ -1,14 +1,11 @@
-var Chess = require('./chess').Chess;
-
 pieces = [wR, wR, wR, wR, wR, wR, wR, wR];
 squares = create_squares();
 rnd_start = randomPositions(pieces, squares);
+
+var Chess = require('./chess').Chess;
 var chess = new Chess(rnd_start);
-chess.ascii(); // show board
 
 while (!fila_y_columna()) { // runs until all rooks on different row and column
-  var moves = chess.moves();
-  var move = moves[Math.floor(Math.random() * moves.length)]; // need to change from random to user input
-  chess.move(move);
+  // play versus person or computer
 }
 console.log(chess.pgn());
