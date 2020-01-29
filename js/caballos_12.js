f503a1a686500d1b1e4e163642dab985bc90a54f
Manuel Morales <manolin@mit.edu>
2020-01-28 22:06:26 -0600

    Update caballos_12.js

--- js/caballos_12.js
+++ js/caballos_12.js
@@ -1,15 +1,12 @@
+var pieces = [wN, wN, wN, wN, wN, wN, wN, wN, wN, wN, wN, wN];
+var squares = create_squares();
+var rnd_start = randomPositions(pieces, squares);
+
 var Chess = require('./chess').Chess;
-
-pieces = [wN, wN, wN, wN, wN, wN, wN, wN, wN, wN, wN, wN];
-squares = create_squares();
-rnd_start = randomPositions(pieces, squares);
 var chess = new Chess(rnd_start);
-chess.ascii(); // show board
 
-solns = ['8/5N2/1NN1NN2/2N5/5N2/2NN1NN1/2N5/8']; // only one soln
+var solns = ['8/5N2/1NN1NN2/2N5/5N2/2NN1NN1/2N5/8']; // only one soln
 while (!solns.includes(chess.fen().slice(0,24))) { // runs until current fen string is a soln
-  var moves = chess.moves();
-  var move = moves[Math.floor(Math.random() * moves.length)]; // need to change from random to user input
-  chess.move(move);
+  // play versus player or computer
 }
 console.log(chess.pgn());
